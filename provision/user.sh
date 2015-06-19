c658ec6aac42cc76771dc90c7a7b59e040b70024
Keegan Quinn <keeganquinn@gmail.com>
2015-06-19 03:32:28 -0700

    Get more code.

--- provision/user.sh
+++ provision/user.sh
@@ -6,7 +6,19 @@
 
 provision="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-git clone -b static-splash git://github.com/bnfinet/ptp-openwrt-files.git
+
+# Personal Telco Project source code.
+git clone git://github.com/personaltelco/ptp-cab.git
+git clone git://github.com/personaltelco/ptp-openwrt-files.git
+git clone git://github.com/personaltelco/ptp-splash-server.git
+git clone git://github.com/personaltelco/ptp-splash-page.git
+
+
+# cwnmyr, the community wireless network management platform.
+git clone git://github.com/keeganquinn/cwnmyr.git
+
+
+# OpenWrt buildroot.
 git clone git://git.openwrt.org/openwrt.git
 
 cd openwrt
